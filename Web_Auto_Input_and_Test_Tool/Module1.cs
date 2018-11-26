662cc091ab03de99bfcf9a65e07ca18594694613
Gorden Hsu <35245730+sosonian@users.noreply.github.com>
2018-11-26 18:41:13 +0800

    Put a sample module (Film Music) in the module class

--- Web_Auto_Input_and_Test_Tool/Module1.cs
+++ Web_Auto_Input_and_Test_Tool/Module1.cs
@@ -9,10 +9,8 @@ namespace Web_Auto_Input_and_Test_Tool
        public class Module1 : InputModule
         {
             public new string  ModuleName = "FilmMusic";
-            
-            public class TitleOfColumn
-            {
-            }
+            public string[,] TitleOfColumn = new string[7,2] {{"NameOfUnit", "單元名稱"},{"NameOfSong","曲名"},{"BGM","配樂"},{"Composing","作曲"},{"Lyric","作詞"},{"MainVocal","主唱"},{"Musician","演奏"}};
+           
             public class InputMainProcess
             {
             }
